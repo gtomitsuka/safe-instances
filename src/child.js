685896af1609bf51a5714ed7cb0bfe26a071f647
Gabriel Tomitsuka <gabriel.tomitsuka@montreus.com>
2015-04-19 19:39:55 +0200

    Update child.js

--- src/child.js
+++ src/child.js
@@ -6,6 +6,6 @@ var assert = require('assert');
 
 var filename = process.argv[2] || 'file.js';
 var file = process.argv[3];
-var timeout = process.argv[4];
+var timeout = new Date(process.argv[4]);
 
-vm.runInThisContext(file, {filename: filename, timeout: 60 * 60}); //Runs for an hour, LOL.
+vm.runInThisContext(file, {filename: filename, timeout: timeout}); //Runs for an hour, LOL.
