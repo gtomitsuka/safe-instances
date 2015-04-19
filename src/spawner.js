2a0269e8f83e6c859d714e5975f2d990abb694d2
Gabriel Tomitsuka <gabriel.tomitsuka@montreus.com>
2015-04-19 19:57:38 +0200

    Update spawner.js

--- src/spawner.js
+++ src/spawner.js
@@ -9,19 +9,22 @@ function Child(location, timeout, filename, encoding, commandType){
   this.commandType = commandType || 'node';
   this.filename = filename;
   this.timeout = timeout || 6 * 60;
+  this._isReady = false;
   fs.readFile(location, this.encoding, function(error, file){
     if(error)
       throw error;
       
-    console.log(file);
     this.file = file;
-    console.log(this.file);
   });
 }
 
 Child.prototype.spawn = function(){
+  if(this._isReady === true){
   var args = ['./child', this.filename, this.file, this.timeout.toString()];
   return child_process.spawn(this.commandType, args, { stdio: ['pipe', 'pipe', 'pipe', 'ipc'] });
+  }else{
+    throw new Error('file is not loaded');
+  }
 }
 
 module.exports = Child;
