14259e920cd10dcd50e6fe6e70ef876325c01770
Gabriel Tomitsuka <gabriel.tomitsuka@montreus.com>
2015-04-19 19:51:58 +0200

    Update child.js

--- src/child.js
+++ src/child.js
@@ -8,4 +8,5 @@ var filename = process.argv[2] || 'child.js';
 var file = process.argv[3];
 var timeout = new Date(process.argv[4]);
 
+console.log(file);
 vm.runInThisContext(file, {filename: filename, timeout: timeout}); //Runs for an hour, LOL.
