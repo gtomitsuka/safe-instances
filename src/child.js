3d5bfdff1913c0e2ead3b2a0a312c578ecba01ca
Gabriel Tomitsuka <gabriel.tomitsuka@montreus.com>
2015-04-20 15:00:26 +0200

    Update child.js

--- src/child.js
+++ src/child.js
@@ -6,5 +6,5 @@ var assert = require('assert');
 
 var file = process.argv[2];
 var timeout = new Date(process.argv[3]);
-console.log(file);
-vm.runInThisContext(file, {timeout: timeout});
+
+vm.runInThisContext(file)//, {timeout: timeout});
