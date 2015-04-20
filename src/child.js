059855554ebed8108b09e4f3982fe44878ddec23
Gabriel Tomitsuka <gabriel.tomitsuka@montreus.com>
2015-04-20 15:35:03 +0200

    Update child.js

--- src/child.js
+++ src/child.js
@@ -7,9 +7,4 @@ var assert = require('assert');
 var file = process.argv[2];
 var timeout = parseInt(process.argv[3], 10);
 
-try {
- vm.runInThisContext(file, {timeout: 1000});
-}
-catch(e) {
-    console.log(e); // Script execution timed out.
-}
+vm.runInThisContext(file, {timeout: 1000});
