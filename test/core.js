c6662c4ded8aa3fcdb8590090817d97f3aca8d0a
Gabriel <gabriel.tomitsuka@montreus.com>
2015-05-22 18:58:42 +0200

    Adds test cases

--- test/core.js
+++ test/core.js
@@ -1,3 +1,20 @@
 //Mocha Tests
+var Child = require('../index');
+var assert = require('assert');
+var child_process = require('child_process');
 
-describe('')
+describe('Pool', function(){
+  it('starts successfully', function(){
+    global.pool = new Child.Pool(2);
+  });
+
+  it('gives a process when available', function(){
+    assert(pool.getProcess().constructor, 'ChildProcess');
+  });
+})
+
+describe('Child', function(){
+  var child;
+
+  //it('')
+})
