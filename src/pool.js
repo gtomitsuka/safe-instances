0e226dac60b8a7da33a2e43e7ed6fd843724316b
Gabriel <gabriel.tomitsuka@montreus.com>
2015-06-02 15:42:19 +0200

    Improves Pool.

--- src/pool.js
+++ src/pool.js
@@ -14,9 +14,26 @@ var util = require('./util');
 //The Pool handles the
 function Pool(size){
   this.processes = [];
+  this.active = [];
 
-  for(var i = 0; i < size; i++)
+  for(var i = 0; i < size; i++){
     this.processes[i] = child_process.spawn(Pool.command, [Pool.file], { stdio: ['pipe', 'pipe', 'pipe', 'ipc']});
+    this.active[i] = false;
+  }
+}
+
+Pool.prototype.getProcess = function(id){
+  if(id != null)
+    return this.processes[id];
+
+  for(var i = 0; i < this.size; i++){
+    if(this.active[i] === false){
+
+      return this.processes[i];
+    }
+  }
+
+  return null;
 }
 
 Pool.prototype.killAll = function(signal){
