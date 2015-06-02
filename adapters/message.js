64e61092314d3c996ed5fa917533d4ec2ec636c9
Gabriel <gabriel.tomitsuka@montreus.com>
2015-06-02 19:52:38 +0200

    Updates adapter's API.

--- adapters/message.js
+++ adapters/message.js
@@ -3,18 +3,20 @@
 /* Adapter: Inter-process Messages
    Description: Uses default, built-in child_process messages. */
 
+var Promise = require('bluebird');
+
 function Adapter(child){
   this.child = child;
 }
 
-Adapter.prototype.init = function(){};
+Adapter.prototype.init = function(){}; //Unnecessary.
 
-Adapter.prototype.contactChild = function(){
-
+Adapter.prototype.contact = function(type, message){
+  return new Promise(function(resolve, reject){
+    this.child.send({type: 'contact', listener: type, message: message});
+  }.bind(this));
 }
 
-Adapter.prototype.messageHandler = function(){ //Handles messages in child process
-
-}.toString();
+Adapter.prototype.contactHandler = function(){}.toString(); //For the Messages Adapter, handling is done by default.
 
 module.exports = Adapter;
