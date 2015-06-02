9d88d8e7aab645dd2350b789aaa9d32ee41fded1
Gabriel <gabriel.tomitsuka@montreus.com>
2015-06-02 16:04:44 +0200

    Creates starting Adapter API.

--- adapters/message.js
+++ adapters/message.js
@@ -2,3 +2,19 @@
 
 /* Adapter: Inter-process Messages
    Description: Uses default, built-in child_process messages. */
+
+function Adapter(child){
+  this.child = child;
+}
+
+Adapter.prototype.init = function(){};
+
+Adapter.prototype.contactChild = function(){
+
+}
+
+Adapter.prototype.messageHandler = function(){ //Handles messages in child process
+
+}.toString();
+
+module.exports = Adapter;
