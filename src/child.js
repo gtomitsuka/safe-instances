30ab5e420e9c64f04c84ca14b5dc46154c2fb385
Gabriel <gabriel.tomitsuka@montreus.com>
2015-06-03 18:17:24 +0200

    API changes.
    Adds global.require if require is allowed.
    Updates events.

--- src/child.js
+++ src/child.js
@@ -12,17 +12,20 @@ global.process.handle = function(message, callback){
 
 
 process.on('message', function(message){
-  if(message.type === 'code'){
+  if(message.type === 'init'){
+    if(message.allowsRequire)
+      global.require = require;
+
     vm.runInThisContext(message.code, {timeout: message.timeout, displayErrors: true});
     return;
   }
 
   if(message.type === 'contact'){
-    function callbackHandler(returnedMessage){
-      process.send({type: 'callback', event: message.event, id: message.id, message: returnedMessage});
+    function callbackHandler(returned){
+      process.send({listener: message.listener, id: message.id, message: returned});
     }
 
-    return _handlers[message.event](param, callbackHandler);
+    return _handlers[message.handler](message.value, callbackHandler);
   }
 
   throw new Error('This kind of message isn\'t currently accepted.')
