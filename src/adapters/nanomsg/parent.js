9d1f95d6f9c933ebffe2a0681ccbc58dc3e3dfc7
Gabriel Tomitsuka <gabriel.tomitsuka@montreus.com>
2015-06-12 14:54:22 +0200

    Update and rename src/adapters/nanomsg.js to src/adapters/nanomsg/parent.js

--- src/adapters/nanomsg/parent.js
+++ src/adapters/nanomsg/parent.js
@@ -1,23 +1,28 @@
 // nanomsg Adapter
+
+//NPM Modules
 var nano = require('nanomsg');
 var Promise = require('bluebird');
 
-var socket;
+//safe-instances Modules
+var util = require('../util');
 
 function Adapter(child){
   this.child = child;
   this.callbacks = {};
-
+  this.socket = nano.socket('req');
+  this.address = 'ipc://safe-instances.local/';
   this.child.process.on('message', function(result){
     this.callbacks[result.id](result.message);
   }.bind(this));
 }
 
 Adapter.prototype.init = function(){
-  socket = nano.socket('req');
-  socket.on('message', function(){
-    req.connect(addr);
-  });
+  this.socket.bind(this.address);
+  this.socket.on('message', function(_result){
+    var result = JSON.stringify(_result.toString())
+    this.callbacks[result.id](result.message);
+  }.bind(this));
 }
 
 Adapter.server = 'child';
