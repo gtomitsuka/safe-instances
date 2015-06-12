fa3fa75f60a60b784d9b1b003d1974e8cbb82380
Gabriel Tomitsuka <gabriel.tomitsuka@montreus.com>
2015-06-12 14:10:26 +0200

    Update nanomsg.js

--- src/adapters/nanomsg.js
+++ src/adapters/nanomsg.js
@@ -2,7 +2,7 @@
 var nano = require('nanomsg');
 var Promise = require('bluebird');
 
-var server;
+var socket;
 
 function Adapter(child){
   this.child = child;
@@ -12,3 +12,13 @@ function Adapter(child){
     this.callbacks[result.id](result.message);
   }.bind(this));
 }
+
+Adapter.prototype.init = function(){
+  socket = nano.socket('req');
+  socket.on('message', function(){
+    req.connect(addr);
+  });
+}
+
+Adapter.server = 'child';
+module.exports = Adapter;
