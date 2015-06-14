0f22055c4ae67a3460dc0c479b5a39f81f495452
Gabriel <gabriel.tomitsuka@montreus.com>
2015-06-14 18:57:05 +0200

    Improves nanomsg adapter

--- src/adapters/nanomsg/parent.js
+++ src/adapters/nanomsg/parent.js
@@ -11,17 +11,32 @@ function Adapter(child){
   this.child = child;
   this.callbacks = {};
   this.socket = nano.socket('req');
-  this.address = 'ipc://safe-instances.local/';
-  this.child.process.on('message', function(result){
+  this.address = 'ipc://safe-instances.local/id/' + util.getRandomInt(1, 999999);
+  this.params = {address: this.address};
+
+  this.socket.on('message', function(_result){
+    var result = JSON.stringify(_result.toString())
     this.callbacks[result.id](result.message);
   }.bind(this));
 }
 
 Adapter.prototype.init = function(){
   this.socket.bind(this.address);
-  this.socket.on('message', function(_result){
-    var result = JSON.stringify(_result.toString())
-    this.callbacks[result.id](result.message);
+}
+
+Adapter.prototype.contact = function(handler, message){
+  return new Promise(function(resolve, reject){
+    var id = util.getRandomInt(1, 999999);
+    var block = {type: 'contact', value: message, handler: handler, id: id};
+
+    function callbackFunction(value){
+      resolve(value);
+
+      this.callbacks[id] = null;
+    }.bind(this)
+
+    this.callbacks[id] = callbackFunction;
+    this.socket.send(block);
   }.bind(this));
 }
 
